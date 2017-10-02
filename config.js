e8077341f997ad3b44b8766d821cfc98d10e0d37
D. Francis <dpf205@gmail.com>
2017-10-02 10:47:29 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	consumer_key:         '5ZR6f9VPmMOKA8ZaQep8dnx1w', // the api key
-	consumer_secret:      'Bb9URjod8E46KjyOkEQjuhdcBReku3eDojVCRg0Q4X3A4wpBuO',
-	access_token:         '842431275650691074-iMGLfulFYHThyXL56kxsmWBGzuiWcjG',
-	access_token_secret:  'u8gqyzcLkNxBXkiAmre8Bm4LAZEfb2yd0YF1jsF5Q5cey',
-	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-};
